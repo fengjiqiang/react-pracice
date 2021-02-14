0cabc04e8034dc5bbddd7e4f91065a3038536999
Ethan Feng <fengjiqiang@inspur.com>
2021-02-14 12:36:14 +0800

    hello react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/prefer-stateless-function */
+// 创建“外壳”组件App
+import React, { Component } from 'react'
+import Hello from './components/Hello'
+import Welcome from './components/Welcome'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 创建并暴露App组件
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+        <Welcome />
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Welcome/index.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react'
+
+export default class Welcome extends PureComponent {
+  render() {
+    return (
+      <h2>Welcome</h2>
+    )
+  }
+}
